ee849e3710a657fc9b7ec4c6864203895f54276c
woongbinni <woongbinni@gmail.com>
2023-05-20 17:22:17 +0900

    [ADD] Step05/JPA Repository Test Code 추가
    - BeverageRepositoryTest
        - whenFindByCostGreaterThan_thenReturnBeveragesGT100
        - whenSaveBeverages_thenReturnCreatedBeverages
    
    - OrderRepositoryTest
        - setUp
        - 초기데이터에_저장된_ID가_100인_ORDER를_조회한다
        - SetUp한_데이터가_조회된다

--- src/test/java/com/sds/icagile/cafe/beverage/BeverageRepositoryTest.java
+++ src/test/java/com/sds/icagile/cafe/beverage/BeverageRepositoryTest.java
@@ -5,39 +5,37 @@ import com.sds.icagile.cafe.beverage.model.BeverageSize;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class BeverageRepositoryTest {
     @Autowired
     private BeverageRepository beverageRepository;
 
-    private Beverage americano;
-    private Beverage latte;
-    private Beverage coldBrew;
-
-    @BeforeEach
-    public void setUp() {
-       // createBeverages();
-    }
-
     @Test
     public void whenFindByCostGreaterThan_thenReturnBeveragesGT100() {
-        //TODO - implement
+        List<Beverage> onlyColdBrew = beverageRepository.findByCostGreaterThan(100);
+
+        assertEquals(onlyColdBrew.size(), 3);
+        assertEquals(onlyColdBrew.get(0).getName(), "americano");
     }
 
     @Test
     public void whenSaveBeverages_thenReturnCreatedBeverages() {
-        //TODO - implement
-    }
+        List<Beverage> newBeverages = beverageRepository.findAll();
 
-    private List<Beverage> createBeverages() {
-        //TODO - implement
-        return null;
+        assertThat(newBeverages.size()).isEqualTo(3);
+        assertThat(newBeverages.get(0).getName()).isEqualTo("americano");
+        assertThat(newBeverages.get(1).getName()).isEqualTo("coldbrew");
+        assertThat(newBeverages.get(2).getName()).isEqualTo("latte");
     }
 }
--- src/test/java/com/sds/icagile/cafe/order/OrderRepositoryTest.java
+++ src/test/java/com/sds/icagile/cafe/order/OrderRepositoryTest.java
@@ -1,11 +1,19 @@
 package com.sds.icagile.cafe.order;
 
+import com.sds.icagile.cafe.order.model.Order;
+import com.sds.icagile.cafe.order.model.OrderStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -16,16 +24,27 @@ public class OrderRepositoryTest {
 
     @BeforeEach
     public void setUp() {
-        //TODO - implement
+        Order waitingOrder = new Order();
+        waitingOrder.setStatus(OrderStatus.WAITING);
+        Order canceledOrder = new Order();
+        waitingOrder.setStatus(OrderStatus.CANCEL);
+
+        orderRepository.saveAll(Arrays.asList(waitingOrder, canceledOrder));
     }
 
     @Test
     public void 초기데이터에_저장된_ID가_100인_ORDER를_조회한다() {
-        //TODO - implement
+        Order order = orderRepository.findById(100).orElse(new Order());
+
+        assertThat(order.getId(), is(100));
+        assertThat(order.getStatus(), is(OrderStatus.PREPARING));
     }
 
     @Test
     public void SetUp한_데이터가_조회된다() {
-        //TODO - implement
+        List<Order> order = orderRepository.findAllByStatus(OrderStatus.CANCEL);
+
+        assertThat(order.size(), is(1));
+        assertThat(order.get(0).getStatus(), is(OrderStatus.CANCEL));
     }
 }
